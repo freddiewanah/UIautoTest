2a1fefa712ab3cad9711df7d274cb3a77d094580
freddie.wanah@gmail.com <han@karma.wiki>
2019-09-01 21:08:58 +1000

    update

--- droidbot-master/scripts/gen.sh
+++ droidbot-master/scripts/gen.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
-printf "Prepare droidbot\n"
-droidbot -a ../apk/wikipedia.apk -o ../data/bot_output/org.wikipedia -is_emulator
+set +e
+for file in ../apk/*; do
+  fileName=${file##*/}
+  baseName=${fileName%.*}
+  echo "${baseName}"
 
-printf "Finished droidbot\n"
+  printf "Prepare droidbot\n"
+  droidbot -a ../apk/${baseName}.apk -o ../data/bot_output/${baseName} -is_emulator
 
-printf "Start apktool\n"
-apktool d ../apk/wikipedia.apk -o ../data/packages/org.wikipedia
-printf "Finished apktool\n"
+  printf "Finished droidbot\n"
 
-printf "run process\n"
-python3 ./process.py org.wikipedia
-printf "Finished process\n"
+  printf "Start apktool\n"
+  apktool d ../apk/${baseName}.apk -o ../data/packages/${baseName}
+  printf "Finished apktool\n"
 
-printf "Start gen html\n"
-python3 ./gen_html.py org.wikipedia
-printf "Finished gen html\n"
+  printf "run process\n"
+  python3 ./process.py ${baseName}
+  printf "Finished process\n"
+
+  printf "Start gen html\n"
+  python3 ./gen_html.py ${baseName}
+  printf "Finished gen html\n"
+
+done
+
+
+
